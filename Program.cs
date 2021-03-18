709db028c29b89d927c4e49a60f41a8a017fc313
rh8512 <ppenza@gmail.com>
2021-03-18 14:28:40 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace csharp_benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var results = BenchmarkRunner.Run<StringReplaceTest>();
+
+            //BenchmarkSwitcher
+            //.FromAssembly(typeof(Program).Assembly)
+            //.Run(args,
+            //    DefaultConfig.Instance
+            //        .With(new EtwProfiler()));
+            //var results = BenchmarkRunner.Run<StringReplaceTest>();
+        }
+    }
+}
